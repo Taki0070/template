d40e9807121ba78c21dd4361937226585ef7f406
user <user@DESKTOP-RK4RUT4>
2024-05-08 18:09:54 +0900

    0

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,49 +1,78 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
+// 関数テンプレート
 template <typename T>
-struct Vec 
+T tMax(T a, T b)
 {
-    T x;
-    T y;
+	if (a > b)
+		return a;
+	else
+		return b;
+
+}
+
+//クラステンプレート
+template <typename T>
+class Vec
+{
+public:
+	T x;
+	T y;
+	
+	T GetX() { return (x); }
+	void SetX(T _x) { x = _x; }
+	void PrintVec() { cout << "(a,b) = " << "(" << x << "," << y << ")" << endl; } //インライン定義
 
 };
 
-template <typename T>
-T length(Vec<T>& vec) 
-{
-    return (vec.x * vec.x + vec.y * vec.y); //長さ計算
-}
-
 
 template <typename T>
-int MaxLen(Vec<T>& a , Vec<T>& b)
+class Vec2
 {
-    T Len = length(a);
-    T Len2 = length(b);
+public:
+	T x;
+	T y;
+	T GetX() { return (x); }
+	void SetX(T _x) { x = _x; }
+	void PrintVec() { cout << "(x , y) = (" << x << " , " << y << ")" << endl; }
+	T Length() const { return (sqrt(x * x + y * y)); }
 
-    if (Len > Len2) 
-        return 1;
-    else
-        return 2;
-}
+	bool operator >(const Vec2<T>& other) {
+		return (this->Length() > other.Length());
+	}
 
-int main() 
+
+};
+
+
+int main()
 {
 
-    Vec<double> vec1 = { 3.0, 4.0 }; 
-    Vec<double> vec2 = { 5.0, 6.0 }; 
+	//// クラステンプレート
+	/*Vec2<double> v;
+	v.x = 2;
+	v.y = 3;
+	v.PrintVec();*/
 
-    int max = MaxLen(vec1, vec2);
+	Vec2<double> v1;
+	v1.x = { 2.3 };
+	v1.y = { 5.0 };
 
-    if (max == 1) 
-    {
-        cout <<" vec1 の方が大きい" << endl;
-    }
-    else if (max == 2) 
-    {
-        cout <<" vec2 の方が大きい" << endl;
-    }
+	Vec2<double> v2;
+	v2.x = { 1.0 };
+	v2.y = { 4.0 };
 
-    return 0;
+	/*if(v1 > v2)
+		v1.PrintVec();
+	else
+		v2.PrintVec();*/
+
+	Vec2<double> res = tMax(v1, v2);
+	res.PrintVec();
+
+	return 0;
 }
